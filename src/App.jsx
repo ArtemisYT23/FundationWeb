ba3b5ab5920060df27157f117da68a761cde66e1
ArtemisYT23 <aqueronte24@gmail.com>
2023-05-09 14:54:06 -0500

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { PublicRoutes } from "./routes/routes";
+import { PagesInitial } from "./pages/PagesInitial";
+import { NavBar } from "./components/NavBar";
 
+function App() {
   return (
-    <>
-      
-    </>
+    <div>
+      <HashRouter>
+      <NavBar />
+        <Routes>
+          <Route path="/" element={<Navigate to={PublicRoutes.initialState} />} />
+          <Route path={PublicRoutes.initialState} element={<PagesInitial />}/>
+        </Routes>
+      </HashRouter>
+    </div>
   )
 }
 
---
+++ src/routes/routes.js
@@ -0,0 +1,7 @@
+export const PublicRoutes = {
+    initialState: "initialState",
+}
+
+export const PrivateRoutes = {
+    private: "private",
+}
